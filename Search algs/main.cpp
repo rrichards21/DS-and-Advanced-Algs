c27adc9a924b852a551b07ad4de67510653fb387
rrichards21 <rodrigo.a.richards@gmail.com>
2020-09-16 23:39:12 -0300

    nada

--- Search algs/main.cpp
+++ Search algs/main.cpp
@@ -6,31 +6,32 @@ using namespace std;
 int main(){
 
   int n;
-  cout<<"Ingrese la cantidad de números"<<endl;
+  cout<<"Cantidad de numeros"<<endl;
   cin>>n;
   Busqueda b(n);
 
 
   int num,pos;
   double time;
-  cout<<"Ingrese número a buscar"<<endl;
+  cout<<"Numero a buscar"<<endl;
   cin>>num;
 
   int rep=100;
-
-  clock_t start = clock();
+  clock_t start;
+  
+  start = clock();
   for(int i=0;i<rep;i++)  pos = b.lineal(num);
-  time = ((double)clock() - start) / CLOCKS_PER_SEC;
+  time = ((double)clock() - start)*1000 / CLOCKS_PER_SEC;
   printf("Lineal: %d\n%.10f\n\n",pos, time/(double)rep);
 
   start = clock();
   for(int i=0;i<rep;i++)  pos = b.binaria(num);
-  time = ((double)clock() - start) / CLOCKS_PER_SEC;
+  time = ((double)clock() - start)*1000 / CLOCKS_PER_SEC;
   printf("Binaria: %d\n%.10f\n\n",pos, time/(double)rep);
 
   start = clock();
   for(int i=0;i<rep;i++)  pos = b.doblada(num);
-  time = ((double)clock() - start) / CLOCKS_PER_SEC;
+  time = ((double)clock() - start)*1000 / CLOCKS_PER_SEC;
   printf("Doblada %d\n%.10f\n\n",pos, time/(double)rep);
     
   return 0;
